b83db891d483dab9982bf2bddea9f1eaf316aed1
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 10:26:01 +0530

    Add files via upload

---
+++ fms9.java
@@ -0,0 +1,12 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms9 {
+    public static void main(String[]args){
+        int l=5;
+        int b=8;
+        int h=6;
+    System.out.println("SurfaceArea="+2*(l*b+b*h+h*1));
+    System.out.println("volume="+1*b*h);
+    }
+    
+}
