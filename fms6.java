dc60536e3c6c1519abc13ab7b437d787ddf0f077
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 09:51:29 +0530

    Add files via upload

---
+++ fms6.java
@@ -0,0 +1,12 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms6 {
+    public static void main(String[]args){
+        int a=7;
+        int b=6;
+        int c=7;
+        int perimeter=a+b+c;
+        System.out.println("Perimeter of triangle="+perimeter);
+    }
+    
+}
