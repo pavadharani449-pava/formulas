9174b432c5ac27c2017795cbefaef20228bdf5a1
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 10:07:13 +0530

    Add files via upload

---
+++ fms8.java
@@ -0,0 +1,15 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms8 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int surfaceArea=7*a;
+        int volume=a*a*a;
+        int perimeter=12*a;
+    System.out.println("SurfaceArea="+surfaceArea);
+    System.out.println("volume="+volume);
+    System.out.println("Perimeter="+perimeter);
+    }
+    
+}
