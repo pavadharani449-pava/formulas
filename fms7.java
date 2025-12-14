d1f8d1f26e0268ffef36383b702adc42e1756a0d
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 09:59:29 +0530

    Add files via upload

---
+++ fms7.java
@@ -0,0 +1,10 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms7{
+    public static void main(String[]args){
+        double radius=7;
+        double Circumference=2*3.14*radius;
+    System.out.println("Circumference of circle="+Circumference);
+    }
+
+}
