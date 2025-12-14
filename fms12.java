323f6f7c6b31fd6c9c1c3a91bd1239ed51e6f77b
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 11:12:30 +0530

    Add files via upload

---
+++ fms12.java
@@ -0,0 +1,10 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms12 {
+    public static  void main(String[]args){
+        double celsius=98.6;
+        double fahrenhelt=(celsius*9/5)+32;
+    System.out.println("Fahrenhelt="+fahrenhelt);
+    }
+    
+}
