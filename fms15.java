e1d25619618ad3709f985252c074ff3329912352
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 11:35:28 +0530

    Add files via upload

---
+++ fms15.java
@@ -0,0 +1,11 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms15 {
+    public static void main(String[]args){
+        double radius=7;
+        double area=3.14*radius*radius;
+    System.out.println("Area of circle="+area);
+    }
+
+    
+}
