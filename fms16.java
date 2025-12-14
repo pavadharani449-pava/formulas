d401c6378188ee57ea4a69c5a322aa660541dbb6
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 11:39:24 +0530

    Add files via upload

---
+++ fms16.java
@@ -0,0 +1,11 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms16 {
+    public static void main(String[]args){
+        int base=10;
+        int height=5;
+        double area=0.5*base*height;
+    System.out.println("Area of Triangle="+area);
+    }
+    
+}
