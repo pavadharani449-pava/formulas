cb2c5620888380a0686388319be02ec29c2b4eb2
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 09:42:39 +0530

    Add files via upload

---
+++ fms4.java
@@ -0,0 +1,12 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms4 {
+    public static void main(String[]args){
+        int side=5;
+        int perimeter=4*side;
+    System.out.println("Perimeter of rectangle="+perimeter);
+
+
+    }
+    
+}
