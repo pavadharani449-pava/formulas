b96fff183d474bad9ae28ccae48db627f7043302
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 09:27:54 +0530

    Add files via upload

---
+++ fms2.java
@@ -0,0 +1,10 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms2 {
+    public static void main(String[]args){
+        int side=5;
+        int perimeter=4*side;
+        System.out.println("Perimeter of square="+perimeter);
+    }
+    
+}
