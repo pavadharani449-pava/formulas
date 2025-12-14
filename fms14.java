438374f5effff8f5d53389e74e7dfe37b307d0a1
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 11:26:01 +0530

    Add files via upload

---
+++ fms14.java
@@ -0,0 +1,11 @@
+package InputOutput.formuls;
+import java.util.Scanner;
+public class fms14 {
+    public static void main(String[]args){
+        int x1=2,y1=3;
+        int x2=5,y2=7;
+        double distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+System.out.println("Distance="+distance);
+    }
+    
+}
